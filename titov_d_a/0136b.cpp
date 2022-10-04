1de3cbb52878f287d1df6aa4b27553265222f59b
Titov Dmitry <dm.a.titov@ya.ru>
2022-10-04 15:50:39 +0300

    dec to tric and tric to dec

--- titov_d_a/0136b.cpp
+++ titov_d_a/0136b.cpp
@@ -1,6 +1,6 @@
 # include <iostream>
 
-std::string to_3(int x) {
+std::string dec_to_tric(int x) {
     std::string r = "";
     while (x > 0) {
         r = (char) ('0' + x % 3) + r;
@@ -9,10 +9,19 @@ std::string to_3(int x) {
     return r;
 }
 
+int tric_to_dec(const std::string& x) {
+    int r = 0, b = 1;
+    for (int i = x.size() - 1; i >= 0; --i) {
+        r += b * (x[i] - '0');
+        b *= 3;
+    }
+    return r;
+}
+
 int main() {
     int a, c;
     std::cin >> a >> c;
-    std::cout << to_3(a) << ' ' << to_3(c);
+    std::cout << tric_to_dec(dec_to_tric(12345)) << '\n';
 
     return 0;
 }
